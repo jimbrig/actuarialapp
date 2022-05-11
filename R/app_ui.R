686ac75eccc7b2f19f289478ea9ae41ddc0c55a4
Jimmy Briggs <jimmy.briggs@pwc.com>
2022-05-11 10:50:15 -0400

    feat: add initial shinyapp functions (resolves #8)

--- R/app_ui.R
+++ R/app_ui.R
@@ -10,7 +10,8 @@ app_ui <- function(request) {
     golem_add_external_resources(),
     # Your application UI logic
     fluidPage(
-      h1("actuarialapp")
+      h1("actuarialapp"),
+      mod_upload_modal_ui("upload_modal_1")
     )
   )
 }
